752ccad5ec91d39416d60f56d3e9fe545197f3d6
holycheet123 <32814987+holycheet123@users.noreply.github.com>
2017-10-22 20:32:05 +0100

    bars

--- Bars.py
+++ Bars.py
@@ -1,20 +1,44 @@
 hot_fire_hard_bars = [
 	"Not to brag, I don't mean to boast, Look, my face is pale, but you look like you've seen a ghost.",
-	"I’m bout to make this mic short circuit. This beat is wack and fits you perfect",
-	"I’m about eight thousand miles away from home, thinking about the throne I got my eyes fixated on",
-
+	"I’m bout to make this mic short circuit. This beat is wack and fits you perfect.",
+	"I’m about eight thousand miles away from home, thinking about the throne I got my eyes fixated on.",
+	"No wonder you're sore now, Lordy you're bored now. I’m pushing 30 you’re breaking 40’s door down.",
+	"Had a spark when you stared but now you're just garbage, fell from 'Top 10' to 'not mentioned at all'.",
+	"I'd like to see things from your point of view, but I can't seem to get my head that far up your ass.",
+	"Do your parents even realize they’re living proof that two wrongs don’t make a right?",
+	"They say music can alter moods and talk to you. Well can it load a gun up for you , and cock it too",
+	""
 ]
 
 mild_fire_medium_bars = [
-
+	"I don’t exactly hate you, but if you were on fire and I had water, I’d drink it.",
+	"Did you know they used to be called 'Jumpolines' until your mum jumped on one?",
+	"Your family tree must be a cactus because everyone on it is a prick.",
+	"It must be difficult for you, exhausting your entire vocabulary in one bar",
+	"Hey- why don't you give me your phone number and I will call you when I give a shit",
+	"Do yourself a favor and ignore anyone who tells you to be yourself. Bad idea in your case.",
+	""
 ]
 
 blend_easy_bars = [
-	"Ayo my pen and paper cause a chain reaction",
-	"You beef with me, i'mma even the score equally",
+	"Ayo my pen and paper cause a chain reaction.",
+	"You beef with me, i'mma even the score equally.",
+	"No, no, im not insulting you, im describing you.",
+	"I could eat a bowl of alphabet soup and shit better bars than you.",
+	"Man, you're so fat. You're the reason why double doors were invented.",
+	"If I wanted to kill myself I’d climb your ego and jump to your IQ.",
+	"You'll never be the man your mother is.",
+	"I'm sorry, was I meant to be offended? The only thing offending is your face",
+	"Someday you'll go far... and I hope you stay there.",
+	"You only annoy me when you’re breathing, really.",
+	""
 
 ]
 legendary_bars = [
-	"His palms are sweaty, knees weak, arms are heavy. There's vomit on his sweater already. Mom's spaghetti",
-	"Now, once upon a time not long ago, there was a little rapper about to blow. But his album came, and it was not good. I think it went lead or double copper-wood"
-]
+	"His palms are sweaty, knees weak, arms are heavy. There's vomit on his sweater already. Mom's spaghetti.",
+	"Now, once upon a time not long ago, there was a little rapper about to blow. But his album came, and it was not good. I think it went lead or double copper-wood.",
+	"Don't ever try to judge me, dude. You don't know what the fuck I've been through. But I know something about you, You went to Cranbrook, that's a private school.",
+	"Your ass forgot, so just in case you don’t remember me, I’ll run your brain around the block to jog your fucking memory",
+	""
+
+
